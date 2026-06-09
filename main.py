65c12617bdf52bec6758d5d0771a0502c0b78164
Breno Gabriel <breno.systems@outlook.com>
2026-06-09 13:14:56 +0000

    Telas de admin, professor, aluno; alteração de horários para períodos e aulas; Adiciona período da tarde

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ st.set_page_config(page_title="Controle de Carrinhos", page_icon="🛒", layout=
 if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
     st.session_state.username = ""
+    st.session_state.role = ""
+    
 
     # Inicializa a conexão com o Neon
 conn = st.connection("postgresql", type="sql")
@@ -15,5 +17,9 @@ conn = st.connection("postgresql", type="sql")
 if __name__ == "__main__":
     if not st.session_state.authenticated:
         login(conn)
-    else:
-        app_principal(conn)
+    elif st.session_state.role == "Administrador":
+        app_admin(conn)
+    elif st.session_state.role == "Professor":
+        app_professor(conn)
+    elif st.session_state.role == "Aluno":
+        app_aluno(conn)
--- utils.py
+++ utils.py
@@ -16,10 +16,13 @@ def login(conn):
             if result:
                 st.session_state.authenticated = True
 
-                query = text("SELECT username FROM users WHERE user_login = :login LIMIT 1")
-                user_name = session.execute(query, {"login": login}).fetchone()[0]
+                query = text("SELECT username, user_role FROM users WHERE user_login = :login LIMIT 1")
+                row = session.execute(query, {"login": login}).fetchone()
+                
+                if row and row[1] in ("Administrador", "Professor", "Aluno"):
+                    st.session_state.role = row[1]
+                    st.session_state.username = row[0]
 
-                st.session_state.username = user_name
                 st.success("Login bem-sucedido!")
                 sleep(1.5)
                 st.rerun()
@@ -27,27 +30,34 @@ def login(conn):
                 st.error("Usuário ou senha inválidos.")
 
 
-def app_principal(conn):
+def app_admin(conn):
 
-    st.title("Controle de Carrinhos")
+    st.title("Controle de Carrinhos - E.E.  Dom Lúcio Antunes")
 
         # Variáveis de ordenação e opções
     day_order = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"]
-    turmas = ["6º A", "6º B", "6º C", "7º A", "7º B", "7º C", "8º A", "8º B", "8º C", "9º A", "9º B", "9º C"]
+    turmas_manha = ["6º A", "6º B", "6º C", "7º A", "7º B", "7º C", "8º A", "8º B", "8º C", "9º A", "9º B", "9º C"]
+    turmas_tarde = ["1º A", "1º B", "2º ADM", "2º B", "3º ADM", "3º B"]
     dias = {"SEG": "Segunda-feira", "TER": "Terça-feira", "QUA": "Quarta-feira", "QUI": "Quinta-feira", "SEX": "Sexta-feira"}
 
         # Formulário de Entrada de Dados
     professor = st.text_input('Professor')
-    turma = st.selectbox('Turma', turmas)
+    periodo = st.selectbox('Período', ['Ensino Fundamental II - Manhã', 'Ensino Médio - Tarde'])
+    aula = st.selectbox('Aula', ['1ª Aula', '2ª Aula', '3ª Aula', '4ª Aula', '5ª Aula', '6ª Aula', '7ª Aula', '8ª Aula'])
+    
+    if periodo == 'Ensino Fundamental II - Manhã':
+        turma = st.selectbox('Turma', turmas_manha)
+    else:
+        turma = st.selectbox('Turma', turmas_tarde)
+    
     dia = st.selectbox('Dia da Semana', list(dias.values()))
 
-    if turma.startswith(("7º", "8º")):
+    if turma.startswith(("7º", "8º", "2º B", "3º B", "1º A")): # Verificar depois
         carrinho = st.selectbox('Carrinho', [1, 3])
     else:
         carrinho = st.selectbox('Carrinho', [2])
 
-    hora_retirada = st.time_input('Horário de Retirada', value=time(7, 0), step=300)
-    hora_entrega = st.time_input('Horário de Entrega', value=time(7, 45), step=300)
+    
 
     # Lógica para Salvar
     if st.button("Salvar"):
@@ -59,14 +69,15 @@ def app_principal(conn):
                 SELECT 1 FROM reservas 
                 WHERE dia = :dia 
                 AND carrinho = :carrinho 
-                AND horario_retirada = :hora_retirada
+                AND periodo = :periodo
+                AND aula = :aula
                 LIMIT 1;
             """)
             
             with conn.session as session:
                 conflito_existe = session.execute(
                     query_conflito, 
-                    {"dia": dia, "carrinho": carrinho, "hora_retirada": hora_retirada}
+                    {"dia": dia, "carrinho": carrinho, "periodo": periodo, "aula": aula}
                 ).fetchone()
 
             if conflito_existe:
@@ -74,16 +85,16 @@ def app_principal(conn):
             else:
                 # INSERT: Envelopado com text()
                 query_insert = text("""
-                    INSERT INTO reservas (dia, horario_retirada, horario_entrega, carrinho, professor, turma)
-                    VALUES (:dia, :hora_retirada, :hora_entrega, :carrinho, :professor, :turma);
+                    INSERT INTO reservas (dia, periodo, aula, carrinho, professor, turma)
+                    VALUES (:dia, :periodo, :aula, :carrinho, :professor, :turma);
                 """)
                 with conn.session as session:
                     session.execute(
                         query_insert,
                         {
                             "dia": dia,
-                            "hora_retirada": hora_retirada,
-                            "hora_entrega": hora_entrega,
+                            "periodo": periodo,
+                            "aula": aula,
                             "carrinho": carrinho,
                             "professor": professor,
                             "turma": turma
@@ -96,7 +107,7 @@ def app_principal(conn):
     # Exibição dos Dados (SELECT)
     try:
         # Usamos conn.session para rodar a query com text() de forma segura
-        query_select = text("SELECT dia, horario_retirada, horario_entrega, carrinho, professor, turma FROM reservas;")
+        query_select = text("SELECT dia, periodo, aula, carrinho, professor, turma FROM reservas;")
         
         with conn.session as session:
             result = session.execute(query_select)
@@ -105,11 +116,48 @@ def app_principal(conn):
         
         if not df_reservas.empty:
             # Ajusta os nomes das colunas para visualização
-            df_reservas.columns = ["Dia", "Horário de Retirada", "Horário de Entrega", "Carrinho", "Professor", "Turma"]
+            df_reservas.columns = ["Dia", "Período", "Aula", "Carrinho", "Professor", "Turma"]
             
             # Ordenação inteligente pelos dias da semana
             df_reservas["Dia"] = pd.Categorical(df_reservas["Dia"], categories=day_order, ordered=True)
-            df_reservas = df_reservas.sort_values(["Dia", "Horário de Retirada", "Turma"]).reset_index(drop=True)
+            df_reservas = df_reservas.sort_values(["Dia", "Período", "Aula"]).reset_index(drop=True)
+            
+            st.subheader("Tabela de Reservas")
+            st.dataframe(df_reservas, hide_index=True)
+        else:
+            st.info("Nenhuma reserva cadastrada ainda.")
+            
+    except Exception as e:
+        st.error(f"Erro ao carregar os dados: {e}")
+
+def app_professor(conn):
+    st.title("Área do Professor - Em Desenvolvimento")
+    st.info("Esta seção está em desenvolvimento. Por favor, aguarde futuras atualizações.")
+
+def app_aluno(conn):
+
+    st.title("Área do Aluno - Em Desenvolvimento")
+    st.info("Esta seção está em desenvolvimento. Por favor, aguarde futuras atualizações.")
+
+    day_order = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"]
+
+    # Exibição dos Dados (SELECT)
+    try:
+        # Usamos conn.session para rodar a query com text() de forma segura
+        query_select = text("SELECT dia, periodo, aula, carrinho, professor, turma FROM reservas;")
+        
+        with conn.session as session:
+            result = session.execute(query_select)
+            # Transforma o resultado do banco em um DataFrame do Pandas
+            df_reservas = pd.DataFrame(result.fetchall(), columns=result.keys())
+        
+        if not df_reservas.empty:
+            # Ajusta os nomes das colunas para visualização
+            df_reservas.columns = ["Dia", "Período", "Aula", "Carrinho", "Professor", "Turma"]
+            
+            # Ordenação inteligente pelos dias da semana
+            df_reservas["Dia"] = pd.Categorical(df_reservas["Dia"], categories=day_order, ordered=True)
+            df_reservas = df_reservas.sort_values(["Dia", "Período", "Aula"]).reset_index(drop=True)
             
             st.subheader("Tabela de Reservas")
             st.dataframe(df_reservas, hide_index=True)
